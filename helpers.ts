80960b545ebec72b3bc795499bf86b5e9b2841cf
cryptohanzo111 <98662159+cryptohanzo111@users.noreply.github.com>
2026-03-07 01:09:10 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T22:08:53.793Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T22:09:10.594Z
+Commit: Fix CSS styling bug on mobile devices
