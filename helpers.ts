0e979155f31c335c56cad2f7eb047026e248f1bb
cryptohanzo111 <98662159+cryptohanzo111@users.noreply.github.com>
2026-03-07 01:30:02 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T22:30:00.693Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T22:30:02.979Z
+Commit: Fix CSS styling bug on mobile devices
